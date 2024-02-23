5373ad62adb4001257ac2f9fff194a17c6779c1e
kijen723 <kij2646@gmail.com>
2024-02-23 09:27:46 +0900

    feat math/PowerSet.java: Implement print

--- src/com/kijen/cs/math/PowerSet.java
+++ src/com/kijen/cs/math/PowerSet.java
@@ -5,20 +5,26 @@ import java.util.ArrayList;
 public class PowerSet {
     static ArrayList<ArrayList<Integer>> subsets = new ArrayList<>();
 
-    private static void powerSet(int[] arr, int start, ArrayList<Integer> subset) {
+    private static void powerSet(int[] arr, int n, int start, ArrayList<Integer> subset) {
         subsets.add(new ArrayList<>(subset));
 
-        for (int i = start; i < arr.length; i++) {
+        for (int i = start; i < n; i++) {
             subset.add(arr[i]);
-            powerSet(arr, i + 1, subset);
+            powerSet(arr, n, i + 1, subset);
             subset.remove(subset.size() - 1);
         }
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
 
-        powerSet(arr, 0, new ArrayList<>());
+        powerSet(arr, n, 0, new ArrayList<>());
+
+        System.out.println("Number of Subsets : 2^n = " + Math.pow(2, n) + " = " + subsets.size());
+        System.out.println();
+
+        System.out.println("Subsets of arr : ");
 
         for (ArrayList<Integer> subset : subsets) System.out.println(subset);
     }
