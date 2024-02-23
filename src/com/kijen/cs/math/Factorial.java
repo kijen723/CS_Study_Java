564f4053fa3b0911e6d387ce2329af7eed953128
kijen723 <kij2646@gmail.com>
2024-02-23 11:47:21 +0900

    style math/Factorial.java

--- src/com/kijen/cs/math/Factorial.java
+++ src/com/kijen/cs/math/Factorial.java
@@ -2,7 +2,9 @@ package com.kijen.cs.math;
 
 public class Factorial {
     public int factorial(int n) {
-        if (n == 1) return 1;
+        if (n == 1) {
+            return 1;
+        }
 
         return n * factorial(n - 1);
     }
