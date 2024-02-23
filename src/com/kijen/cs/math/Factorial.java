19edca6db9501468da9599d86730770029ac1551
kijen723 <kij2646@gmail.com>
2024-02-23 11:28:08 +0900

    create math/Factorial.java

---
+++ src/com/kijen/cs/math/Factorial.java
@@ -0,0 +1,17 @@
+package com.kijen.cs.math;
+
+public class Factorial {
+    public int factorial(int n) {
+        if (n == 1) return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Factorial fc = new Factorial();
+
+        int n = 5;
+
+        System.out.println("n = " + n + ", n! = " + fc.factorial(n));
+    }
+}
