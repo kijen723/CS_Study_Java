0ad72745aa20807e5f39c715e4a3681ce700cac7
kijen723 <kij2646@gmail.com>
2024-02-23 11:23:04 +0900

    fix math/PowerSet.java: Objectification

--- src/com/kijen/cs/math/PowerSet.java
+++ src/com/kijen/cs/math/PowerSet.java
@@ -3,29 +3,49 @@ package com.kijen.cs.math;
 import java.util.ArrayList;
 
 public class PowerSet {
-    static ArrayList<ArrayList<Integer>> subsets = new ArrayList<>();
+    private final ArrayList<ArrayList<Integer>> subsets = new ArrayList<>();
 
-    private static void powerSet(int[] arr, int n, int start, ArrayList<Integer> subset) {
+    public PowerSet(int[] arr, int n) {
+        powerSet(arr, n, 0, new ArrayList<>());
+    }
+
+    private void powerSet(int[] arr, int n, int start, ArrayList<Integer> subset) {
         subsets.add(new ArrayList<>(subset));
 
         for (int i = start; i < n; i++) {
             subset.add(arr[i]);
             powerSet(arr, n, i + 1, subset);
-            subset.remove(subset.size() - 1);
+            // subset.remove(subset.size() - 1); // Until Java 10
+            subset.removeLast();
         }
+    }
+
+    public ArrayList<ArrayList<Integer>> get() {
+        return subsets;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        for (ArrayList<Integer> subset : subsets) {
+            result.append(subset).append("\n");
+        }
+
+        if (!result.isEmpty()) {
+            result.deleteCharAt(result.length() - 1);
+        }
+
+        return result.toString();
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int n = arr.length;
 
-        powerSet(arr, n, 0, new ArrayList<>());
+        PowerSet ps = new PowerSet(arr, n);
 
-        System.out.println("Number of Subsets : 2^n = " + Math.pow(2, n) + " = " + subsets.size());
-        System.out.println();
-
-        System.out.println("Subsets of arr : ");
-
-        for (ArrayList<Integer> subset : subsets) System.out.println(subset);
+        System.out.println("Number of Subsets : 2^n = " + (int) Math.pow(2, n) + " = " + ps.get().size() + "\n");
+        System.out.println("Subsets of arr : \n" + ps);
     }
 }
