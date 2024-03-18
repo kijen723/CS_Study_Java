0e026fc97085eb32222cb2eeaa4ac496bc3366bf
kijen723 <kij2646@gmail.com>
2024-03-18 17:32:30 +0900

    create java/MyHashMap

---
+++ src/com/kijen/java/MyHashMap.java
@@ -0,0 +1,11 @@
+package com.kijen.java;
+
+import java.util.HashMap;
+
+public class MyHashMap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("a", 1);
+    }
+}
