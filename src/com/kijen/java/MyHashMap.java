de5703f339a1f2074f29d7dbdb57c521330a91e8
kijen723 <kij2646@gmail.com>
2024-03-20 21:18:37 +0900

    fix java/MyHashMap

--- src/com/kijen/java/MyHashMap.java
+++ src/com/kijen/java/MyHashMap.java
@@ -7,5 +7,22 @@ public class MyHashMap {
         HashMap<String, Integer> map = new HashMap<>();
 
         map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 1);
+        map.put("d", 2);
+
+        map.remove("a");
+        System.out.println(map);
+        System.out.println(map.get("b"));
+
+        for (String value : map.keySet()) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        for (Integer value : map.values()) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
     }
 }
